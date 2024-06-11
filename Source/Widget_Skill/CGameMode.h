dceabb111e19dc70ade13250c77e1094fc7c501d
ken <ken7131@gmail.com>
2024-06-11 21:20:48 +0900

    New Commit

---
+++ Source/Widget_Skill/CGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CGameMode.generated.h"
+
+UCLASS()
+class WIDGET_SKILL_API ACGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Widget_Skill/Widget_Skill.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Widget_Skill.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Widget_Skill, "Widget_Skill" );
+ 
